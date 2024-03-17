3a9cdd0c3d37ed9d319b0b98714d77a88282173c
Delienelu310 <seredoha.nets@gmail.com>
2024-03-17 10:49:59 +0100

    Bot: implement preflop strategy

--- pokerengine/src/main/java/com/ilumusecase/bot/TightAgressiveStrategy.java
+++ pokerengine/src/main/java/com/ilumusecase/bot/TightAgressiveStrategy.java
@@ -4,9 +4,11 @@ package com.ilumusecase.bot;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.Random;
 import java.util.HashMap;
 
 import com.ilumusecase.game.Action;
+import com.ilumusecase.game.ActionType;
 import com.ilumusecase.game.Card;
 import com.ilumusecase.game.Player;
 import com.ilumusecase.game.Round;
@@ -18,12 +20,108 @@ public class TightAgressiveStrategy implements PlayerStrategy {
 
     @Override
     public Action getAction(Round round, Player player) {
-        
+
+        Integer stageNumber = getStageNumber(round);
+        if(stageNumber.equals(0)) return preflopStrategy(round, player);
+
+
         List<Card> bestHand = compareHandsMethod.findBestHand(round, player.getPokerHand());
 
+        Integer handPower = compareHandsMethod.getHandPower(bestHand);
+        List<Integer> ranks = compareHandsMethod.getRanks(bestHand);
+
         return null;
+    }
 
+    private Action preflopStrategy(Round round, Player player){
+        /*
+            If no bet and if (ranks number + pos number > 26) then bet, or if bet then call
+            Else if pair/ the same suit + close ranks then call (bb or bet)
+            10% - 3bet with pair or samesuit+closeranks
+            if ranks number > 25 then call
+            if pair and rank > 10 then 4bet and all in  
+        */
+        Integer maxBet = 0;
+        for(Player pl : round.getPlayersLeft()){
+            if(pl.getCurrentBet() > maxBet) maxBet = pl.getCurrentBet();
+        }
 
+        Integer ranksSum = player.getPokerHand().get(0).getRank() + player.getPokerHand().get(1).getRank();
+        Integer posNumber = round.getPlayers().indexOf(player);
+        Boolean isPair = player.getPokerHand().get(0).getRank().equals(player.getPokerHand().get(1).getRank());
+        Integer pairRank = player.getPokerHand().get(0).getRank();
+        Boolean isDrawingHand = player.getPokerHand().get(0).getSuit().equals(player.getPokerHand().get(1).getSuit())
+            && Math.abs(player.getPokerHand().get(0).getRank() - player.getPokerHand().get(1).getRank()) < 2;
+
+        Random random = new Random();
+
+        if(maxBet * 1.0 / round.getBlindSize() < 2.5){
+            if(ranksSum + posNumber > 26 || isPair && pairRank >= 10){
+                Action action =  new Action();
+                action.setSize(maxBet * 2 - player.getCurrentBet());
+                action.setActionType(ActionType.RAISE);
+                action.setPlayer(player);
+                return action;
+            }else if(isPair || isDrawingHand){
+                Action action =  new Action();
+                action.setSize(maxBet - player.getCurrentBet());
+                action.setActionType(ActionType.CALL);
+                action.setPlayer(player);
+                return action;
+            }else{
+                Action action = new Action();
+                action.setActionType(ActionType.FOLD);
+                action.setPlayer(player);
+                return action;
+            }
+        }else if(maxBet * 1.0 / round.getBlindSize() > 2.5 & maxBet * 1.0 / round.getBlindSize() < 5.5){
+            if(ranksSum > 25 || isPair && pairRank >= 10 || random.nextDouble() < 0.1 && isDrawingHand){
+                Action action =  new Action();
+                action.setSize(maxBet * 2 - player.getCurrentBet());
+                action.setActionType(ActionType.RAISE);
+                action.setPlayer(player);
+                return action;
+            }else if(isPair || isDrawingHand){
+                Action action =  new Action();
+                action.setSize(maxBet - player.getCurrentBet());
+                action.setActionType(ActionType.CALL);
+                action.setPlayer(player);
+                return action;
+            }else{
+                Action action = new Action();
+                action.setActionType(ActionType.FOLD);
+                action.setPlayer(player);
+                return action;
+            }
+        }else{
+            if(isPair && pairRank >= 12 && player.getCurrentBet() + player.getBankroll() > maxBet){
+                Action action = new Action();
+                action.setSize(player.getBankroll() - player.getCurrentBet());
+                action.setActionType(ActionType.RAISE);
+                action.setPlayer(player);
+                return action;
+            }else if(ranksSum >= 26){
+                Action action = new Action();
+                action.setSize(maxBet - player.getCurrentBet());
+                action.setActionType(ActionType.CALL);
+                action.setPlayer(player);
+                return action;
+            }else{
+                Action action = new Action();
+                action.setActionType(ActionType.FOLD);
+                action.setPlayer(player);
+                return action;
+            }
+        }
+    }
+
+    private Integer getStageNumber(Round round){
+        Integer result = 0;
+        for(Action action : round.getActions()){
+            if(action.getActionType().equals(ActionType.NEXTSTAGE)) result++;
+        }
+
+        return result;
     }
 
     private Integer getBetFillingMoney(Round round, Player player){
@@ -165,8 +263,6 @@ public class TightAgressiveStrategy implements PlayerStrategy {
         return result;
     }
 
-    
-    
     
     
 }
